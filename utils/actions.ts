72d51c7e59b26cb01a0151a1a8b63cd2cc104e4d
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-10-01 23:01:17 +0200

    first commit

---
+++ utils/actions.ts
@@ -0,0 +1,44 @@
+import db from "@/utils/db";
+import { redirect } from "next/navigation";
+
+export const fetchFeaturedProducts = async () => {
+  // possibly using the values here, await them and store into a variable
+  const products = await db.product.findMany({
+    where: {
+      featured: true,
+    },
+    // select: {
+    //   name: true,
+    // },
+  });
+  return products;
+};
+
+export const fetchAllProducts = ({ search = "" }: { search: string }) => {
+  // not using the fetched values, just return the result to the calling code
+  return db.product.findMany({
+    where: {
+      OR: [
+        {
+          name: { contains: search, mode: "insensitive" },
+        },
+        {
+          company: { contains: search, mode: "insensitive" },
+        },
+      ],
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
+
+export const fetchSingleProduct = async (productId: string) => {
+  const product = await db.product.findUnique({
+    where: {
+      id: productId,
+    },
+  });
+  if (!product) redirect("/products");
+  return product;
+};
