96fae75d9ab25865d897f7e11746ac755442efde
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-10-02 16:36:13 +0200

    .

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import db from "@/utils/db";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export const fetchFeaturedProducts = async () => {
@@ -49,5 +50,35 @@ export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return { message: "product created" };
+  // TODO: access clerkId with helper functions
+  const user = await currentUser();
+  // temp solution to obtain the user
+  if (!user) redirect("/");
+  try {
+    // manually fetch the inputs
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    // will be dealt with Zod
+    const price = Number(formData.get("price") as string);
+    // temp (will be stored on a supabase bucket)
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/product-1.jpg", // hardcode for time being
+        description,
+        featured,
+        clerkId: user.id, // check if the user exists
+      },
+    });
+
+    return { message: "product created" };
+  } catch (error) {
+    return { message: "there was an error" };
+  }
 };
