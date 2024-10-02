e84727ee9935a92a8a5656d61da460378f41813e
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-10-02 11:43:53 +0200

    .

--- components/form/PriceInput.tsx
+++ components/form/PriceInput.tsx
@@ -1,5 +1,27 @@
-function PriceInput() {
-  return <div>PriceInput</div>;
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+
+const name = "price";
+type FormInputNumberProps = {
+  defaultValue?: number;
+};
+
+function PriceInput({ defaultValue }: FormInputNumberProps) {
+  return (
+    <div className="mb-2">
+      <Label htmlFor={name} className="capitalize">
+        Price ($)
+      </Label>
+      <Input
+        id={name}
+        type="number"
+        name={name}
+        min={0}
+        defaultValue={defaultValue || 100}
+        required
+      />
+    </div>
+  );
 }
 
 export default PriceInput;
