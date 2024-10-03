6e95fc6d51685826db8230d85b6e6d0bda57d337
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-10-03 13:50:27 +0200

    .

--- components/reviews/Rating.tsx
+++ components/reviews/Rating.tsx
@@ -1,5 +1,29 @@
-function Rating() {
-  return <div>Rating</div>;
+import { FaStar, FaRegStar } from "react-icons/fa";
+
+function Rating({ rating }: { rating: number }) {
+  // rating = 2
+  // 1 <= 2 true
+  // 2 <= 2 true
+  // 3 <= 2 false
+  // 4 <= 2 false
+  // 5 <= 2 false
+  const stars = Array.from({ length: 5 }, (_, i) => {
+    return i + 1 <= rating;
+  });
+  return (
+    <div className="flex items-center gap-x-1">
+      {stars.map((isFilled, i) => {
+        const className = `w-3 h-3 ${
+          isFilled ? "text-primary" : "text-grey-400"
+        }`;
+        return isFilled ? (
+          <FaStar key={i} className={className} />
+        ) : (
+          <FaRegStar key={i} className={className} />
+        );
+      })}
+    </div>
+  );
 }
 
 export default Rating;
