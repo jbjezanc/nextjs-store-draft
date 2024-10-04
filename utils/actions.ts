636c75ec35426ca66411511a46f4f9f8d88e2d4c
Josip Bjezancevic <josip.bjezancevic@gmail.com>
2024-10-04 10:33:01 +0200

    .

--- utils/actions.ts
+++ utils/actions.ts
@@ -479,7 +479,30 @@ export const removeCartItemAction = async (
   }
 };
 
-export const updateCartItemAction = async () => {};
+export const updateCartItemAction = async ({
+  amount,
+  cartItemId,
+}: {
+  amount: number;
+  cartItemId: string;
+}) => {
+  const user = await getAuthUser();
+  try {
+    const cart = await fetchOrCreateCart({
+      userId: user.id,
+      errorOnFailure: true,
+    });
+    await db.cartItem.update({
+      where: { id: cartItemId, cartId: cart.id },
+      data: { amount },
+    });
+    await updateCart(cart);
+    revalidatePath("/cart");
+    return { message: "cart updated" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
 
 export const createOrderAction = async (prevState: any, formData: FormData) => {
   return { message: "order created" };
